87f11ccae17be913452a6ad0e6247395680db2a6
DockDock <ndockman@comcast.net>
2023-05-24 15:43:42 -0500

    Added dashboard.handlebars, helpers in utils folder. Modified homeRoutes.js.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,38 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 //const withAuth = require('../utils/auth');
 
+router.get("/", (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+    return;
+  }
+  else {
+    res.render("/dashboard");
+  }
+});
 
+//how does this route work? Why is it in homeRoutes?
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        }
+      ]
+    });
 
+    const post = postData.get({ plain: true });
 
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
@@ -18,6 +47,7 @@ router.get('/login', (req, res) => {
       return;
     }
   
+    // why render login at this point?
     //res.render('login');
   });
 
--- models/Post.js
+++ models/Post.js
@@ -11,7 +11,7 @@ Post.init(
             primaryKey: true,
             autoIncrement: true
         },
-        title: {
+        headline: {
             type: DataTypes.STRING,
             allowNull: false
         },
